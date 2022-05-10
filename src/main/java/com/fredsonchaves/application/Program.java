33edfbadf20e801f22e2a636ab1f83db4ac0838b
fredsonchaves07 <fredsonchaves07@gmail.com>
2022-05-10 13:32:17 -0300

    feat: reusing instantiation

--- src/main/java/com/fredsonchaves/application/Program.java
+++ src/main/java/com/fredsonchaves/application/Program.java
@@ -1,22 +1,15 @@
 package com.fredsonchaves.application;
 
-import com.fredsonchaves.db.DB;
-import com.fredsonchaves.db.DBException;
-import com.fredsonchaves.db.DBIntegrityException;
 import com.fredsonchaves.models.dao.DaoFactory;
 import com.fredsonchaves.models.dao.SellerDAO;
-import com.fredsonchaves.models.entities.Department;
 import com.fredsonchaves.models.entities.Seller;
-
-import java.sql.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 public class Program {
 
     public static void main(String[] args) {
 
         SellerDAO sellerDAO = DaoFactory.createSellerDao();
+        System.out.println("=== TEST #1: seller findById ===");
         Seller seller = sellerDAO.findById(3);
         System.out.println(seller);
     }
--- src/main/java/com/fredsonchaves/models/dao/impl/SellerJDBC.java
+++ src/main/java/com/fredsonchaves/models/dao/impl/SellerJDBC.java
@@ -50,16 +50,8 @@ public class SellerJDBC implements SellerDAO {
             preparedStatement.setInt(1, id);
             resultSet = preparedStatement.executeQuery();
             if (resultSetIsNext(resultSet)) {
-                Department department = new Department();
-                department.setId(resultSet.getInt("DepartmentId"));
-                department.setName(resultSet.getString("DepName"));
-                Seller seller = new Seller();
-                seller.setId(resultSet.getInt("Id"));
-                seller.setName(resultSet.getString("Name"));
-                seller.setEmail(resultSet.getString("Email"));
-                seller.setBaseSalary(resultSet.getDouble("BaseSalary"));
-                seller.setBirthDate(resultSet.getDate("BirthDate"));
-                seller.setDepartment(department);
+                Department department = instantiateDepartament(resultSet);
+                Seller seller = instantiateSeller(resultSet, department);
                 return seller;
             }
 
@@ -70,6 +62,24 @@ public class SellerJDBC implements SellerDAO {
             DB.closeResultSet(resultSet);
         }
         return null;
+    }
+
+    private Seller instantiateSeller(ResultSet resultSet, Department department) throws SQLException {
+        Seller seller = new Seller();
+        seller.setId(resultSet.getInt("Id"));
+        seller.setName(resultSet.getString("Name"));
+        seller.setEmail(resultSet.getString("Email"));
+        seller.setBaseSalary(resultSet.getDouble("BaseSalary"));
+        seller.setBirthDate(resultSet.getDate("BirthDate"));
+        seller.setDepartment(department);
+        return seller;
+    }
+
+    private Department instantiateDepartament(ResultSet resultSet) throws SQLException {
+        Department department = new Department();
+        department.setId(resultSet.getInt("DepartmentId"));
+        department.setName(resultSet.getString("DepName"));
+        return department;
     }
 
     @Override
