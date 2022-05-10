41310ec1684dc5275e46cecaef7b3f6ccb63dcfa
fredsonchaves07 <fredsonchaves07@gmail.com>
2022-05-10 18:18:06 -0300

    feat: insert seller

--- src/main/java/com/fredsonchaves/application/Program.java
+++ src/main/java/com/fredsonchaves/application/Program.java
@@ -5,6 +5,7 @@ import com.fredsonchaves.models.dao.SellerDAO;
 import com.fredsonchaves.models.entities.Department;
 import com.fredsonchaves.models.entities.Seller;
 
+import java.sql.Date;
 import java.util.List;
 
 public class Program {
@@ -22,5 +23,9 @@ public class Program {
         System.out.println("\n=== TEST #3: seller findAll ===");
         sellers = sellerDAO.findAll();
         System.out.println(sellers);
+        System.out.println("\n=== TEST #4: seller insert ===");
+        seller = new Seller(null, "Gred", "greg@email.com", new Date(1995, 5, 10), 4000.0, department);
+        sellerDAO.insert(seller);
+        System.out.println("Inserted! New id = " + seller.getId());
     }
 }
--- src/main/java/com/fredsonchaves/models/dao/impl/SellerJDBC.java
+++ src/main/java/com/fredsonchaves/models/dao/impl/SellerJDBC.java
@@ -6,10 +6,7 @@ import com.fredsonchaves.models.dao.SellerDAO;
 import com.fredsonchaves.models.entities.Department;
 import com.fredsonchaves.models.entities.Seller;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -25,7 +22,34 @@ public class SellerJDBC implements SellerDAO {
 
     @Override
     public void insert(Seller seller) {
-
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = connection.prepareStatement("""
+                        INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId)
+                        VALUES (?, ?, ?, ?, ?)
+                     """,
+                    Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, seller.getName());
+            preparedStatement.setString(2, seller.getEmail());
+            preparedStatement.setDate(3, new java.sql.Date(seller.getBirthDate().getTime()));
+            preparedStatement.setDouble(4, seller.getBaseSalary());
+            preparedStatement.setInt(5, seller.getDepartment().getId());
+            int rowsAffected = preparedStatement.executeUpdate();
+            if (rowsAffected > 0) {
+                ResultSet resultSet = preparedStatement.getGeneratedKeys();
+                if (resultSet.next()) {
+                    int id = resultSet.getInt(1);
+                    seller.setId(id);
+                }
+                DB.closeResultSet(resultSet);
+            } else {
+                throw new DBException("Unexpected error! No rows affected!");
+            }
+        } catch (SQLException error) {
+            throw new DBException(error.getMessage());
+        } finally {
+            DB.closeStatement(preparedStatement);
+        }
     }
 
     @Override
